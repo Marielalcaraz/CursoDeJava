6741a6e21b7ed6d3e7aa1fe27c5b20e367066137
Marielalcaraz <111131085+Marielalcaraz@users.noreply.github.com>
2022-08-24 21:44:02 -0300

    Create EjercicioCombinado.java

---
+++ EjercicioCombinado.java
@@ -0,0 +1,21 @@
+package Ejercicio3;
+
+public class Main {
+    public static void main(String[] args) {
+        float num1=6;
+        float num2 = 5;
+        float num3 =2;
+        float num4 =3;
+        float num5 = 7;
+        float num6 = 30;
+        float num7 = 1;
+        float mult = (num3 /num4) * (num5 / num3);
+        float suma = num3 / num6;
+        float resta = num1 / num2;
+        float op = resta - mult + suma;
+        float den = (num7 / num4) / 5;
+        float fin = op / den;
+        System.out.println(fin);
+    }
+
+}
